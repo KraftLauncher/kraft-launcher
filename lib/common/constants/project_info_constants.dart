a84a4e64ba95367b752ff0c043ce92bee7ee4624
Ellet <echo.ellet@gmail.com>
2025-05-09 18:16:27 +0300

    chore: update website link

--- lib/common/constants/project_info_constants.dart
+++ lib/common/constants/project_info_constants.dart
@@ -2,8 +2,7 @@ import '../generated/pubspec.g.dart';
 
 abstract final class ProjectInfoConstants {
   static const displayName = 'Kraft Launcher';
-  static const website =
-      'https://github.com/KraftLauncher/kraft-launcher/blob/main/README.md';
+  static const website = 'https://kraftlauncher.org';
   static const contactEmail = 'kraftlauncher@gmail.com';
 
   static const githubRepoLink = Pubspec.repository;
