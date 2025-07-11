f80b0848ccd4e167012f3628bd78eb2ab1969183
Ellet <echo.ellet@gmail.com>
2025-07-11 16:33:15 +0300

    chore: add new TODOs

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -12,6 +12,8 @@ import 'package:path_provider/path_provider.dart';
 //  because Enum.byName is a built-in, more efficient, and safer way to get enum
 //  values by their name string. It avoids manual iteration and potential errors.
 // TODO: Read: https://dart.dev/tools/linter-rules/avoid_slow_async_io, review all usages of file sync operations
+// TODO: Avoid using @visibleForTesting for private fields
+// TODO: Consider handling all errors/exceptions thrown from any fromJson()?
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
