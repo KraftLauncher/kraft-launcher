cdfb5eda99652807fa00151705fae90e9666dc7f
Ellet <echo.ellet@gmail.com>
2025-08-24 11:00:07 +0300

    chore: create collection_utils package

---
+++ packages/collection_utils/lib/collection_utils.dart
@@ -0,0 +1,66 @@
+import 'dart:math' as math show Random;
+
+/// Similar to https://api.flutter.dev/flutter/foundation/listEquals.html
+/// but does not require Flutter to work.
+bool listEquals<T>(List<T> list1, List<T> list2) {
+  if (identical(list1, list2)) {
+    return true;
+  }
+  if (list1.length != list2.length) {
+    return false;
+  }
+
+  for (int i = 0; i < list1.length; i++) {
+    if (list1[i] != list2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+extension ListX<T> on List<T> {
+  /// Returns the new index to focus on after an element is removed from a list.
+  ///
+  /// If the removed index is still within bounds, returns it (the next item takes its place).
+  /// If the removed item was the last, returns the previous index if available.
+  /// Returns `null` if the list is empty.
+  ///
+  /// NOTE: This should be called on the new list with the removal, not the current list.
+  int? getNewIndexAfterRemoval(int removedIndex) {
+    if (isEmpty) {
+      return null;
+    }
+    if (removedIndex < length) {
+      return removedIndex;
+    }
+    if (removedIndex - 1 >= 0) {
+      return removedIndex - 1;
+    }
+    return null;
+  }
+
+  /// Returns the new focus element after an element was removed from the list.
+  ///
+  /// The new focus element is the next element if it exist, or the element before the current,
+  /// or `null` if the list is empty.
+  ///
+  /// NOTE: This should be called on the new list with the removal, not the current list.
+  T? getReplacementElementAfterRemoval(int removedIndex) {
+    if (isEmpty) {
+      return null;
+    }
+
+    final newIndex = getNewIndexAfterRemoval(removedIndex);
+    if (newIndex != null) {
+      return this[newIndex];
+    }
+    return null;
+  }
+
+  int? indexWhereOrNull(bool Function(T item) test) {
+    final index = indexWhere(test);
+    return index == -1 ? null : index;
+  }
+
+  T get randomElement => this[math.Random().nextInt(length)];
+}
